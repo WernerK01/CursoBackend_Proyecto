aaedea752c827eda4341a45c871684ef2c6fae9b
Werner Koll <wernerkoll01@gmail.com>
2025-11-02 01:41:48 -0300

    Delete old files

--- src/old/products/realTimeProducts.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Product } from '../../core/ProductsManager.js';
-const realTimeProductRoute = express.Router();
-
-realTimeProductRoute.get('/', (req, res) => {
-    res.status(200).render('realTimeProducts', { title: 'Real Time Products' });
-});
-
-export default realTimeProductRoute;
